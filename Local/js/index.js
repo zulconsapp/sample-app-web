0e76c09ba518bd81ebddde5b348d3c09cb88c146
zulconsapp <zulfikar09@users.noreply.github.com>
2020-04-01 12:19:46 +0800

    Update index.js

--- Local/js/index.js
+++ Local/js/index.js
@@ -78,30 +78,30 @@ document.getElementById('join_meeting').addEventListener('click', (e) => {
 
 
 getSignature(meetConfig) {
-	// fetch('${YOUR_SIGNATURE_ENDPOINT}', {
-	// 		method: 'POST',
-	// 		body: JSON.stringify({ meetingData: meetConfig })
-	// 	})
-  //   .then(result => result.text())
-	// 	.then(response => {
-	// 		ZoomMtg.init({
-	// 			leaveUrl: meetConfig.leaveUrl,
-	// 			isSupportAV: true,
-	// 			success: function() {
-	// 				ZoomMtg.join({
-	// 					signature: response,
-	// 					apiKey: meetConfig.apiKey,
-	// 					meetingNumber: meetConfig.meetingNumber,
-	// 					userName: meetConfig.userName,
-	// 					// Email required for Webinars
-	// 					userEmail: meetConfig.userEmail,
-	// 					// password optional; set by Host
-	// 					password: meetConfig.password,
-	// 					error(res) {
-	// 						console.log(res)
-	// 					}
-	// 				})
-	// 			}
-	// 		})
-	// }
+	fetch('${YOUR_SIGNATURE_ENDPOINT}', {
+			method: 'POST',
+			body: JSON.stringify({ meetingData: meetConfig })
+		})
+    .then(result => result.text())
+		.then(response => {
+			ZoomMtg.init({
+				leaveUrl: meetConfig.leaveUrl,
+				isSupportAV: true,
+				success: function() {
+					ZoomMtg.join({
+						signature: response,
+						apiKey: meetConfig.apiKey,
+						meetingNumber: meetConfig.meetingNumber,
+						userName: meetConfig.userName,
+						// Email required for Webinars
+						userEmail: meetConfig.userEmail,
+						// password optional; set by Host
+						password: meetConfig.password,
+						error(res) {
+							console.log(res)
+						}
+					})
+				}
+			})
+	}
 }
