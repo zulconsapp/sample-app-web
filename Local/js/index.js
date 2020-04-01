83f8e16fd00ea8862b0639781c3070dc26cf2158
zulconsapp <zulfikar09@users.noreply.github.com>
2020-04-02 01:31:05 +0800

    Update index.js

--- Local/js/index.js
+++ Local/js/index.js
@@ -1,5 +1,5 @@
-//import { ZoomMtg } from '@zoomus/websdk'
-import { ZoomMtg } from 'zoomus-jssdk'
+import { ZoomMtg } from '@zoomus/websdk'
+// import { ZoomMtg } from 'zoomus-jssdk'
 
 const zoomMeeting = document.getElementById("zmmtg-root")
 
@@ -75,33 +75,31 @@ document.getElementById('join_meeting').addEventListener('click', (e) => {
 });
 
 
-
-
-getSignature(meetConfig) {
-	fetch('${YOUR_SIGNATURE_ENDPOINT}', {
-			method: 'POST',
-			body: JSON.stringify({ meetingData: meetConfig })
-		})
-    .then(result => result.text())
-		.then(response => {
-			ZoomMtg.init({
-				leaveUrl: meetConfig.leaveUrl,
-				isSupportAV: true,
-				success: function() {
-					ZoomMtg.join({
-						signature: response,
-						apiKey: meetConfig.apiKey,
-						meetingNumber: meetConfig.meetingNumber,
-						userName: meetConfig.userName,
-						// Email required for Webinars
-						userEmail: meetConfig.userEmail,
-						// password optional; set by Host
-						password: meetConfig.password,
-						error(res) {
-							console.log(res)
-						}
-					})
-				}
-			})
-	}
-}
+// getSignature (meetConfig) {
+// 	fetch('${YOUR_SIGNATURE_ENDPOINT}', {
+// 			method: 'POST',
+// 			body: JSON.stringify({ meetingData: meetConfig })
+// 		})
+// 		.then(result => result.text())
+// 		.then(response => {
+// 			ZoomMtg.init({
+// 				leaveUrl: meetConfig.leaveUrl,
+// 				isSupportAV: true,
+// 				success: function() {
+// 					ZoomMtg.join({
+// 						signature: response,
+// 						apiKey: meetConfig.apiKey,
+// 						meetingNumber: meetConfig.meetingNumber,
+// 						userName: meetConfig.userName,
+// 						// Email required for Webinars
+// 						userEmail: meetConfig.userEmail,
+// 						// password optional; set by Host
+// 						password: meetConfig.password
+// 						error(res) {
+// 							console.log(res)
+// 						}
+// 					})
+// 				}
+// 			})
+// 	}
+// }
