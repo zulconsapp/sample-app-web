7d50c41b5e31c88becab57bb3a59ba4f85804a30
zulconsapp <zulfikar09@users.noreply.github.com>
2020-04-01 12:10:42 +0800

    Update index.js

--- Local/js/index.js
+++ Local/js/index.js
@@ -1,24 +1,27 @@
-import { ZoomMtg } from '@zoomus/websdk';
+//import { ZoomMtg } from '@zoomus/websdk'
+import { ZoomMtg } from 'zoomus-jssdk'
+
+const zoomMeeting = document.getElementById("zmmtg-root")
 
 console.log('checkSystemRequirements');
 console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
 
 // it's option if you want to change the WebSDK dependency link resources. setZoomJSLib must be run at first
 // if (!china) ZoomMtg.setZoomJSLib('https://source.zoom.us/1.7.2/lib', '/av'); // CDN version default
-// else ZoomMtg.setZoomJSLib('https://jssdk.zoomus.cn/1.7.2/lib', '/av'); // china cdn option 
+// else ZoomMtg.setZoomJSLib('https://jssdk.zoomus.cn/1.7.2/lib', '/av'); // china cdn option
 // ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareJssdk();
 
 
-const API_KEY = 'YOUR_API_KEY';
+const API_KEY = 'PgDAooqbQ4GeQb2gpimUAw';
 
 /**
     * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
     * The below generateSignature should be done server side as not to expose your api secret in public
     * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
     */
-const API_SECRET = 'YOUR_API_SECRET';
+const API_SECRET = 'fsIGoqi8UU2wJJsAjn9VBOHf96xqV4FH65Ua';
 
 document.getElementById('join_meeting').addEventListener('click', (e) => {
     e.preventDefault();
@@ -32,6 +35,8 @@ document.getElementById('join_meeting').addEventListener('click', (e) => {
         leaveUrl: 'https://zoom.us',
         role: 0
     };
+
+
 
     ZoomMtg.generateSignature({
         meetingNumber: meetConfig.meetingNumber,
@@ -68,3 +73,35 @@ document.getElementById('join_meeting').addEventListener('click', (e) => {
         }
     });
 });
+
+
+
+
+getSignature(meetConfig) {
+	// fetch('${YOUR_SIGNATURE_ENDPOINT}', {
+	// 		method: 'POST',
+	// 		body: JSON.stringify({ meetingData: meetConfig })
+	// 	})
+  //   .then(result => result.text())
+	// 	.then(response => {
+	// 		ZoomMtg.init({
+	// 			leaveUrl: meetConfig.leaveUrl,
+	// 			isSupportAV: true,
+	// 			success: function() {
+	// 				ZoomMtg.join({
+	// 					signature: response,
+	// 					apiKey: meetConfig.apiKey,
+	// 					meetingNumber: meetConfig.meetingNumber,
+	// 					userName: meetConfig.userName,
+	// 					// Email required for Webinars
+	// 					userEmail: meetConfig.userEmail,
+	// 					// password optional; set by Host
+	// 					password: meetConfig.password,
+	// 					error(res) {
+	// 						console.log(res)
+	// 					}
+	// 				})
+	// 			}
+	// 		})
+	// }
+}
