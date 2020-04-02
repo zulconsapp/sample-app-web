9c02aa4756661223309fd3a6c4af5694cd0a63ba
zulconsapp <zulfikar09@users.noreply.github.com>
2020-04-02 11:31:26 +0800

    Update index.js

--- Local/js/index.js
+++ Local/js/index.js
@@ -1,43 +1,32 @@
-import { ZoomMtg } from '@zoomus/websdk'
-// import { ZoomMtg } from 'zoomus-jssdk'
-
+import { ZoomMtg } from '@zoomus/websdk';
 const zoomMeeting = document.getElementById("zmmtg-root")
-
+ZoomMtg.setZoomJSLib('node_modules/@zoomus/websdk/dist/lib', '/av');
 console.log('checkSystemRequirements');
 console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
-
 // it's option if you want to change the WebSDK dependency link resources. setZoomJSLib must be run at first
 // if (!china) ZoomMtg.setZoomJSLib('https://source.zoom.us/1.7.2/lib', '/av'); // CDN version default
 // else ZoomMtg.setZoomJSLib('https://jssdk.zoomus.cn/1.7.2/lib', '/av'); // china cdn option
 // ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareJssdk();
-
-
 const API_KEY = 'PgDAooqbQ4GeQb2gpimUAw';
-
 /**
     * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
     * The below generateSignature should be done server side as not to expose your api secret in public
     * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
     */
 const API_SECRET = 'fsIGoqi8UU2wJJsAjn9VBOHf96xqV4FH65Ua';
-
 document.getElementById('join_meeting').addEventListener('click', (e) => {
     e.preventDefault();
-
     const meetConfig = {
         apiKey: API_KEY,
         apiSecret: API_SECRET,
         meetingNumber: parseInt(document.getElementById('meeting_number').value, 10),
         userName: document.getElementById('display_name').value,
         passWord: '',
-        leaveUrl: 'https://zoom.us',
+        leaveUrl: 'http://localhost:9999/index.html',
         role: 0
     };
-
-
-
     ZoomMtg.generateSignature({
         meetingNumber: meetConfig.meetingNumber,
         apiKey: meetConfig.apiKey,
@@ -75,7 +64,8 @@ document.getElementById('join_meeting').addEventListener('click', (e) => {
 });
 
 
-getSignature (meetConfig) {
+
+getSignature(meetConfig) {
 	fetch('${YOUR_SIGNATURE_ENDPOINT}', {
 			method: 'POST',
 			body: JSON.stringify({ meetingData: meetConfig })
@@ -103,3 +93,20 @@ getSignature (meetConfig) {
 			})
 	}
 }
+
+
+const crypto = require('crypto') // crypto comes with Node.js
+
+function generateSignature(apiKey, apiSecret, meetingNumber, role) {
+
+  // Prevent time sync issue between client signature generation and zoom
+  const timestamp = new Date().getTime() - 30000
+  const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString('base64')
+  const hash = crypto.createHmac('sha256', apiSecret).update(msg).digest('base64')
+  const signature = Buffer.from(`${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`).toString('base64')
+
+  return signature
+}
+
+// pass in your Zoom JWT API Key, Zoom JWT API Secret, Zoom Meeting Number, and 0 to join meeting or webinar or 1 to start meeting
+console.log(generateSignature(process.env.API_KEY, process.env.API_SECRET, 123456789, 0))
